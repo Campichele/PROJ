8e4c913a391644db68d8c73eeb34df267181eddb
Campichele <leandre.campiche@etml-es.ch>
2020-05-11 22:46:51 +0200

    aa

---
+++ Dev/prototype-MTG-film-dev/quickstart/src/Api/Data/MovieToGoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Api.Models;
+
+namespace Api.Data
+{
+    public class MovieToGoContext : DbContext
+    {
+        public MovieToGoContext (DbContextOptions<MovieToGoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Api.Models.Movie> Movie { get; set; }
+    }
+}
