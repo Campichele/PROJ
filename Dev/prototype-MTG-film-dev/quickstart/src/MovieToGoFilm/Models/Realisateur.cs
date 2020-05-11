704c3e5bec88856a522094863119b1bb39d27287
Campichele <leandre.campiche@etml-es.ch>
2020-05-11 19:17:44 +0200

    dev

---
+++ Dev/prototype-MTG-film-dev/quickstart/src/MovieToGoFilm/Models/Realisateur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieToGoFilm.Models
+{
+    public partial class Realisateur
+    {
+        public Realisateur()
+        {
+            Realise = new HashSet<Realise>();
+        }
+
+        public short IdRealisateur { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+        public virtual ICollection<Realise> Realise { get; set; }
+    }
+}
