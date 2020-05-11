b58e271793815c1eb9562ecf8e7df894ae628441
Campichele <leandre.campiche@etml-es.ch>
2020-05-11 19:15:56 +0200

    fuckkkk

---
+++ Dev/prototype-MTG-film/quickstart/src/MovieToGoFilm/Models/Wishlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieToGoFilm.Models
+{
+    public partial class Wishlist
+    {
+        public short IdWishlist { get; set; }
+        public short IdFilm { get; set; }
+        public short IdUser { get; set; }
+
+        public virtual Film IdFilmNavigation { get; set; }
+        public virtual Utilisateur IdUserNavigation { get; set; }
+    }
+}
