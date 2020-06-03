e4d31c4236da24fcf7781a4fc413f007af034516
Campichele <leandre.campiche@etml-es.ch>
2020-06-03 18:12:09 +0200

    plzplzpl

---
+++ Dev/MovieToGo-New-lc/API/Models/Distributeur.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    [Table("DISTRIBUTEUR")]
+    public partial class Distributeur
+    {
+        public Distributeur()
+        {
+            Film = new HashSet<Film>();
+        }
+
+        [Key]
+        [Column("ID_DISTRIBUTEUR")]
+        public short IdDistributeur { get; set; }
+        [Required]
+        [Column("NOM")]
+        [StringLength(32)]
+        public string Nom { get; set; }
+
+        [InverseProperty("IdDistributeurNavigation")]
+        public virtual ICollection<Film> Film { get; set; }
+    }
+}
---
+++ Dev/MovieToGo-New-lc/API/Models/Droit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    [Table("DROIT")]
+    public partial class Droit
+    {
+        public Droit()
+        {
+            Utilisateur = new HashSet<Utilisateur>();
+        }
+
+        [Key]
+        [Column("ID_DROIT")]
+        public short IdDroit { get; set; }
+        [Required]
+        [Column("NOM")]
+        [StringLength(32)]
+        public string Nom { get; set; }
+
+        [InverseProperty("IdDroitNavigation")]
+        public virtual ICollection<Utilisateur> Utilisateur { get; set; }
+    }
+}
