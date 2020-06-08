f1ddb4f4a725270c28887954f44fa93f952859d4
Max <pichonnama@gmail.com>
2020-06-08 15:41:01 +0200

    Mange User role

---
+++ Dev/MovieToGoNew-8.6.20 - Maxime/MovieToGo-New/API/Controllers/FilmsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmsController : ControllerBase
+    {
+        private readonly MovieToGoDbContext _context;
+
+        public FilmsController(MovieToGoDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Films
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Film>>> GetFilms()
+        {
+            return await _context.Film.ToListAsync();
+        }
+
+        // GET: api/Films/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Film>> GetFilm(short id)
+        {
+            var film = await _context.Film.FindAsync(id);
+
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return film;
+        }
+
+        // PUT: api/Films/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFilm(short id, Film film)
+        {
+            if (id != film.IdFilm)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(film).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FilmExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Films
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Film>> PostFilm(Film film)
+        {
+            _context.Film.Add(film);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFilm", new { id = film.IdFilm }, film);
+        }
+
+        // DELETE: api/Films/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Film>> DeleteFilm(short id)
+        {
+            var film = await _context.Film.FindAsync(id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            _context.Film.Remove(film);
+            await _context.SaveChangesAsync();
+
+            return film;
+        }
+
+        private bool FilmExists(short id)
+        {
+            return _context.Film.Any(e => e.IdFilm == id);
+        }
+    }
+}
---
+++ Dev/MovieToGoNew-8.6.20 - Maxime/MovieToGo-New/API/Controllers/NotesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly MovieToGoDbContext _context;
+
+        public NotesController(MovieToGoDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Notes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Note>>> GetNotes()
+        {
+            return await _context.Note.ToListAsync();
+        }
+
+        // GET: api/Notes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Note>>> GetNote(short id)
+        {
+            var ToutesNotes = await _context.Note.ToListAsync();
+            ActionResult<IEnumerable<Note>> note = null;
+
+            if (ToutesNotes == null)
+            {
+                return NotFound();
+            }
+
+            foreach (var item in ToutesNotes)
+            {
+                if (item.IdFilm == id)
+                {
+                    //note.ToList().Add(item);
+                    note.Value.ToList().Add(item);
+                }
+            }
+
+            return note;
+        }
+
+        // PUT: api/Notes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNote(short id, Note note)
+        {
+            if (id != note.IdNote)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(note).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NoteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Notes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Note>> PostNote(Note note)
+        {
+            _context.Note.Add(note);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNote", new { id = note.IdNote }, note);
+        }
+
+        // DELETE: api/Notes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Note>> DeleteNote(short id)
+        {
+            var note = await _context.Note.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            _context.Note.Remove(note);
+            await _context.SaveChangesAsync();
+
+            return note;
+        }
+
+        private bool NoteExists(short id)
+        {
+            return _context.Note.Any(e => e.IdNote == id);
+        }
+    }
+}
