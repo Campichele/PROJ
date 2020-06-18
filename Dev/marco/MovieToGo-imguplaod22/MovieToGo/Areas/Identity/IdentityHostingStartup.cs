a0e25540b2cd0ddf66814f6012f939350166d06f
Marcoleboss <61978290+Marcoleboss@users.noreply.github.com>
2020-06-18 11:09:54 +0200

    uplo

---
+++ Dev/marco/MovieToGo-imguplaod22/MovieToGo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MovieToGo.Models;
+
+[assembly: HostingStartup(typeof(MovieToGo.Areas.Identity.IdentityHostingStartup))]
+namespace MovieToGo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
